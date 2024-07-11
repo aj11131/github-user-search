333fe52c2e8f49959b959f10bf43c6cdf675fff2
aj11131 <austin.scott.j@gmail.com>
2024-07-11 18:39:52 -0500

    Add fontawesome

--- src/app/github-users/user-row/user-row.component.ts
+++ src/app/github-users/user-row/user-row.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { GithubUser } from '../../types/github-user';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-user-row',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './user-row.component.html',
   styleUrl: './user-row.component.scss'
 })
 export class UserRowComponent {
+  faArrowUpRightFromSquare = faArrowUpRightFromSquare;
+
   @Input() user!: GithubUser;
 }
