a0ee469441097f174b7b5c438b730bc0f17d294c
aj11131 <austin.scott.j@gmail.com>
2024-07-12 09:33:08 -0500

    Change main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'github-users', loadComponent: () => import('./github-users/github-users.component').then(m => m.GithubUsersComponent)},
+    {path: '', loadComponent: () => import('./github-users/github-users.component').then(m => m.GithubUsersComponent)},
     {path: '**', redirectTo: 'github-users'}
 ];
